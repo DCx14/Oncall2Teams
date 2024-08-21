fc64d0370a835f57b2666b5d676b75726eafd9d0
Dylan <31109734+DCx14@users.noreply.github.com>
2024-08-21 15:54:19 +0200

    Update receiver.php for workflow teams

--- receiver.php
+++ receiver.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // set UTC FR
 setlocale(LC_TIME, 'fr_FR');
@@ -21,17 +21,50 @@ if (json_last_error()) {
     return;
 }
 
-//get metric in webhook
 
+
+
+//get metric in webhook
 
 
 $severity = $my_array['alert_payload']['alerts'][0]['labels']['severity'];
 $alertname = $my_array['alert_payload']['alerts'][0]['labels']['alertname'];
-$status = $my_array['alert_payload']['alerts'][0]['status']; 
+$status = $my_array['alert_payload']['alerts'][0]['status'];
 $Description = $my_array['alert_payload']['alerts'][0]['annotations']['description'];
 $UrlAlert = $my_array['alert_group']['permalinks']['web'];
 $Annotations = $my_array['alert_payload']['alerts'][0]['annotations'];
 $WebhookDest = $headers['Webhook-Url'] ; // Get URL destination
+$Namespaces = $my_array['alert_payload']['alerts'][0]['labels']['namespace'];
+$Pod = $my_array['alert_payload']['alerts'][1]['labels']['pod'];
+
+if (!$Pod){
+  $Pod ="N/A";
+}
+if (!$Namespaces){
+  $Namespaces = "N/A";
+}
+if ($status){
+  $status = $status . " " ."🚨";
+}
+
+$facts = [
+    [
+        "title" => "Status",
+        "value" => $status
+    ],
+    [
+        "title" => "Alert Name",
+        "value" => $alertname
+    ]
+];
+
+// Add annotations to facts array
+foreach ($Annotations as $key => $value) {
+    $facts[] = [
+        "title" => $key,
+        "value" => $value
+    ];
+}
 
 
 // function for tranform json
@@ -46,7 +79,6 @@ function arrayToTextWithNewlines($array) {
 
 // Utilisation de la fonction
 $AnnotationsBrut = arrayToTextWithNewlines($Annotations);
-
 $AnnotationsBr = nl2br($AnnotationsBrut);
 
 // condition for change color and images
@@ -55,16 +87,20 @@ if ($severity === "critical")
 {
     $images = 'https://cdn0.iconfinder.com/data/icons/basic-11/97/9-512.png';
     $color = '#EA0601';
+    $style = "attention";
 
 }elseif ($severity === "warning")
 {
     $images = 'https://cdn4.iconfinder.com/data/icons/set-1/32/__23-256.png';
     $color = '#E06C10';
+    $style = "warning";
+
 }elseif  ($severity === "info")
 {
     $images = 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678110-sign-info-256.png';
     $color = '#1D6BE0';
-
+    $style = "emphasis";
+  
 }else{
 
     $images = 'https://cdn1.iconfinder.com/data/icons/web-illustration-1/132/48-256.png';
@@ -73,49 +109,210 @@ if ($severity === "critical")
 
 
 $data = [
-            '@type'      => 'MessageCard',
-            '@context'   => 'https://schema.org/extensions',
-            'summary'    => 'AlertManager',
-            'themeColor' => $color,
-            'title'      => 'Alert: ' . $alertname,
-            'sections'   => [
-                [
-                    'activityTitle'    => $status,
-                    'activitySubtitle' => date('Y-m-d H:i:s'),
-                    'activityImage'    => $images,
-                    'facts'            => [
-                        [
-                            'name'  => 'Description: ',
-                            'value' => $Description
+    "type" => "message",
+    "attachments" => [
+        [
+            "contentType" => "application/vnd.microsoft.card.adaptive",
+            "content" => [
+                "$schema" => "http://adaptivecards.io/schemas/adaptive-card.json",
+                "type" => "AdaptiveCard",
+                
+                "version" => "1.0",
+                "msteams" => [
+                    "width" => "Full"
+                ],
+                "body" => [
+                    [
+                        "type" => "Container",
+                        "style" => $style,
+                        "items" => [
+                            [ 
+                                "type" => "ColumnSet",
+                                "columns" => [
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "size" => "large",
+                                                "weight" => "bolder",
+                                                "text" => "**Alert Grafana**",
+                                                "style" => "heading",
+                                                "horizontalAlignment" => "center",
+                                                "wrap" => true
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ],
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "Image",
+                                                "url" => $images,
+                                                "altText" => "Pending",
+                                                "height" => "30px"
+                                            ]
+                                        ],
+                                        "width" => "auto"
+                                    ]
+                                ]
+                            ]
                         ],
-                        [
-                            'name'  => 'severity',
-                            'value' => $severity
-                        ],
-                        [
-                            'name'  => 'Annotations',
-                            'value' => $AnnotationsBr
-                        ],
-
+                        "bleed" => true
                     ],
-                ]
-            ],
-            'potentialAction' => [
-                [
-                    '@type' => 'OpenUri',
-                    'name' => 'Accéder a l\'alerte',
-                    'targets' => [
-                        [
-                            'os' => 'default',
-                            'uri' => $UrlAlert
+                    [
+                        "type" => "FactSet",
+                        "facts" => $facts // Insert the dynamically generated facts here
+                    ],
+                    [
+                        "type" => "TextBlock",
+                        "wrap" => true
+                    ],
+                    [
+                        "type" => "TextBlock",
+                        "text" => " "
+                    ],
+                    [
+                        "type" => "TextBlock",
+                        "text" => "   "
+                    ],
+                    [
+                        "type" => "TextBlock",
+                        "text" => "   "
+                    ],
+                    [
+                        "type" => "Table",
+                        "columns" => [
+                            [
+                                "width" => 1
+                            ],
+                            [
+                                "width" => 1
+                            ],
+                            [
+                                "width" => 1
+                            ],
+                            [
+                                "width" => 1
+                            ]
                         ],
-                 ]
-               ]
-               
-               
-             ],
-         ];
-        
+                        "rows" => [
+                            [
+                                "type" => "TableRow",
+                                "cells" => [
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "Nom du Namespace",
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ],
+
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "Nom du Pod",
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ],
+                            [
+                                "type" => "TableRow",
+                                "cells" => [
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => $Namespaces,
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ],
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => $Pod,
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ],
+                                ]
+                            ],
+                            [
+                                "type" => "TableRow",
+                                "cells" => [
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "",
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ],
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "",
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ],
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "",
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ],
+                                    [
+                                        "type" => "TableCell",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "",
+                                                "wrap" => true
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ],
+                        "showGridLines" => false
+                    ],
+                    [
+                        "type" => "ActionSet",
+                        "actions" => [
+                            [
+                                "type" => "Action.OpenUrl",
+                                "title" => "Voir le détail sur grafana",
+                                "URL" => $UrlAlert
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
+
 $json_data1 = json_encode( $data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
 
 // Send WebHook
